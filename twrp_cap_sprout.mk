17498c90684d4c3c61e1b5b9158a0587115b4aa2
Carlo Dee <carlodee.official@proton.me>
2024-01-15 15:42:59 +0800

    cap_sprout: Switch to base makefile.
    * Since we are only building recoveries, full_base_telephony (most likely for building custom roms only) is not necessary.
    
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- twrp_cap_sprout.mk
+++ twrp_cap_sprout.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
